cf7e67ffdafb5e7f5bea925ab70ac982dcdf8e15
Radanmes Lima <radanmes@gmail.com>
2025-03-23 20:27:19 -0300

    PersonClass

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name : str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
+
+    def __repr__(self) -> str:
+        return f"Person(name={self.name}, age={self.age})"
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    [Person(person_dict["name"], person_dict["age"]) for person_dict in people]
+
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
+
+        wife_name = person_dict.get("wife")
+        husband_name = person_dict.get("husband")
+        if wife_name:
+            person.wife = Person.people.get(wife_name)
+        if husband_name:
+            person.husband = Person.people.get(husband_name)
+    return list(Person.people.values())
